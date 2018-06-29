93812394cd771042722ab1d659f223464b229dd0
Richard Memory <richardfmemory@gmail.com>
2018-06-29 11:25:20 -0600

    Progress

--- public/js/angular/todoCtrl.js
+++ public/js/angular/todoCtrl.js
@@ -4,14 +4,44 @@
 		.module('myApp')
 		.controller('TodosController', TodosController)
 	TodosController.$inject = [
-		"$scope"
+		"$scope",
+		"$http"
 	];
 	function TodosController(
-		$scope
+		$scope,
+		$http
 	) {
+
 		$scope.todos = [
-			{ body: 'Go to store' },
-			{ body: 'Finish Video' },
 		];
+		// $scope.todos = [
+		// 	{ body: 'Go to store', completed: true },
+		// 	{ body: 'Finish Video', completed: false },
+		// 	{ body: 'Learn Angular', completed: false },
+		// ];
+
+		$http.get('/todos').success(function(todos) {
+			$scope.todos = todos;
+		});
+
+		$scope.remaining = function() {
+			var count = 0;
+
+			angular.forEach($scope.todos, function(todo) {
+				count += todo.completed ? 0 : 1;
+			});
+
+			return count;
+		}
+
+		$scope.addTodo = function() {
+			var todo = {
+				body: $scope.newTodoText,
+				completed: false
+			};
+
+			$scope.todos.push(todo);
+			$http.post('todos', todo);
+		}
 	}
 })();
--- resources/views/welcome.php
+++ resources/views/welcome.php
@@ -5,13 +5,31 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Example of Angular JS and Laravel</title>
+	<style>
+		small {
+			font-size: .8em;
+			color: grey;
+		}
+	</style>
 </head>
 <body ng-controller="TodosController">
-	<h1>Todos header</h1>
+	<h1>
+		Todos header
+		<small ng-if="remaining()">([[ remaining() ]] remaining)</small>
+	</h1>
 
+	<input type="text" placeholder="Filter todos" ng-model="search">
 	<ul>
-		<li ng-repeat="todo in todos">[[todo.body]]</li>
+		<li ng-repeat="todo in todos | filter:search">
+			<input type="checkbox" ng-model="todo.completed"/>
+			[[todo.body]]
+		</li>
 	</ul>
+
+	<form ng-submit="addTodo()">
+		<input type="text" placeholder="Add new task" ng-model="newTodoText">
+		<button type="submit">Add Task</button>
+	</form>
 </body>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.7.2/angular.min.js"></script>
 	<script src="/js/angular/app.js"></script>
